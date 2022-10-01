538badba5ceb939e5e7dfc2ed1ae0105e8e409aa
CarolinaMaza <caromaza1595@gmail.com>
2022-10-01 20:08:29 -0300

    Creación de carpeta FrontEnd y BackEnd

---
+++ Portfolio/BackEnd/CMaza/src/main/java/com/portfolio/CMaza/CMazaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.CMaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CMazaApplication.class, args);
+	}
+
+}
