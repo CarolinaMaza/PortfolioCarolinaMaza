ab486c12978947799ecfe5c2f5db754dfd8200a2
CarolinaMaza <caromaza1595@gmail.com>
2022-10-07 21:15:14 -0300

    Relación frontend y backend

--- Portfolio/BackEnd/CMaza/src/main/java/com/portfolio/CMaza/Controller/PersonaController.java
+++ Portfolio/BackEnd/CMaza/src/main/java/com/portfolio/CMaza/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.CMaza.Entity.Persona;
 import com.portfolio.CMaza.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
+@CrossOrigin(origins= "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -34,7 +36,7 @@ public class PersonaController {
         return "La persona fue eliminada correctamente";
     }
     
-    // URL:PUERTO/personas/editar/id?nombre="NuevoNombre"&apellido="NuevoApellido"&img="NuevoString"
+    // URL:PUERTO/personas/editar/id?nombre=NuevoNombre&apellido=NuevoApellido&img=NuevoString
     @PutMapping("/personas/editar/(id)")
     public Persona editarPersona(@PathVariable Long id,
                                  @RequestParam ("nombre") String nuevoNombre,
@@ -48,6 +50,12 @@ public class PersonaController {
         ipersonaService.guardarPersona (persona);
         return persona;
     }
+    
+    @GetMapping ("/persona/traer/perfil")
+    public Persona traerpersona(){
+        return ipersonaService.traerPersona((long)1);
+    }
+            
     
 }
     
